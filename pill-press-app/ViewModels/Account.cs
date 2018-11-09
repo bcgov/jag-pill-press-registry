1fc7f16ff39b369a461e6403ca6636dcedc4db66
George Walker <george@walkersoftware.ca>
2018-11-09 14:45:55 -0800

    SPDPP-152 - Business Profile Portal API

--- pill-press-app/ViewModels/Account.cs
+++ pill-press-app/ViewModels/Account.cs
@@ -39,18 +39,34 @@ namespace Gov.Jag.PillPressRegistry.Public.ViewModels
         YT
     }
 
+    public enum BusinessTypeEnum
+    {
+        [Display(Name = "Sole Proprietor")]
+        SoleProprietor = 1,
+        [Display(Name = "Society")]
+        Society = 931490000,
+        [Display(Name = "Partnership")]
+        Partnership = 931490001,
+        [Display(Name = "Company")]
+        Company = 931490002
+    }
+
     public class Account
     {
-        public string id { get; set; }
-        public string name { get; set; } //dynamics = name
+        public string id { get; set; } // Guid
+
+        public string clientId { get; set; } // dynamics = accountnumber
+
+        public string businessLegalName { get; set; } // dynamics = name
+        public string doingBusinessAs { get; set; } // dynamics = bcgov_doingbusinessasdbaname
+        public string businessNumber { get; set; } // dynamics = bcgov_businessnumber
+        public string businessType { get; set; } // dynamics = businesstypecode
+
         public string description { get; set; } //dynamics = description
         public string externalId { get; set; }
-        public string bcIncorporationNumber { get; set; } //dynamics = adoxio_bcincorporationnumber
-        public DateTimeOffset? dateOfIncorporationInBC { get; set; } //dynamics = adoxio_dateofincorporationinbc
-        public string businessNumber { get; set; } //dynamics = accountnumber
-        public string pstNumber { get; set; } //dynamics = adoxio_pstnumber
-        public string contactEmail { get; set; } //dynamics = emailaddress1
-        public string contactPhone { get; set; } //dynamics = telephone1
+                
+        public string businessEmail { get; set; } //dynamics = emailaddress1
+        public string businessPhone { get; set; } //dynamics = telephone1
         public string mailingAddressName { get; set; } //dynamics = address1_name
         public string mailingAddressStreet { get; set; } //dynamics = address1_line1
         public string mailingAddressCity { get; set; } //dynamics = address1_city
@@ -59,65 +75,6 @@ namespace Gov.Jag.PillPressRegistry.Public.ViewModels
         public string mailingAddresPostalCode { get; set; } //dynamics = address1_postalcode
 
         public ViewModels.Contact primarycontact { get; set; }
-
-        public string businessType { get; set; }
-
-        public bool isCorporateDetailsComplete(AdoxioApplicantTypeCodes? legalentitytype, bool corporateDetailsFilesExists)
-        {
-            var isComplete = false;
-            var tiedHouse = new ViewModels.TiedHouseConnection();
-            switch (legalentitytype)
-            {
-                case AdoxioApplicantTypeCodes.PrivateCorporation:
-                case AdoxioApplicantTypeCodes.PublicCorporation:
-                case AdoxioApplicantTypeCodes.UnlimitedLiabilityCorporation:
-                case AdoxioApplicantTypeCodes.LimitedLiabilityCorporation:
-                case AdoxioApplicantTypeCodes.Society:
-                    isComplete = !string.IsNullOrEmpty(bcIncorporationNumber) &&
-                        !string.IsNullOrEmpty(businessNumber) &&
-                        (dateOfIncorporationInBC != null) &&
-                        !string.IsNullOrEmpty(contactEmail) &&
-                        !string.IsNullOrEmpty(contactPhone) &&
-                        !string.IsNullOrEmpty(mailingAddressName) &&
-                        !string.IsNullOrEmpty(mailingAddressStreet) &&
-                        !string.IsNullOrEmpty(mailingAddressCity) &&
-                        !string.IsNullOrEmpty(mailingAddressCountry) &&
-                        (mailingAddressProvince != null) &&// TODO: This field should be a string(by Moffat)
-                        !string.IsNullOrEmpty(mailingAddresPostalCode) &&
-                        corporateDetailsFilesExists;
-
-                    break;
-                case AdoxioApplicantTypeCodes.SoleProprietor:
-                    isComplete = !string.IsNullOrEmpty(businessNumber) &&
-                        !string.IsNullOrEmpty(contactEmail) &&
-                        !string.IsNullOrEmpty(contactPhone) &&
-                        !string.IsNullOrEmpty(mailingAddressName) &&
-                        !string.IsNullOrEmpty(mailingAddressStreet) &&
-                        !string.IsNullOrEmpty(mailingAddressCity) &&
-                        !string.IsNullOrEmpty(mailingAddressCountry) &&
-                        (mailingAddressProvince != null) &&
-                        !string.IsNullOrEmpty(mailingAddresPostalCode);
-                    break;
-                case AdoxioApplicantTypeCodes.GeneralPartnership:
-                case AdoxioApplicantTypeCodes.LimitedLiabilityPartnership:
-                case AdoxioApplicantTypeCodes.LimitedPartnership:
-                    isComplete = !string.IsNullOrEmpty(businessNumber) &&
-                        !string.IsNullOrEmpty(contactEmail) &&
-                        !string.IsNullOrEmpty(contactPhone) &&
-                        !string.IsNullOrEmpty(mailingAddressName) &&
-                        !string.IsNullOrEmpty(mailingAddressStreet) &&
-                        !string.IsNullOrEmpty(mailingAddressCity) &&
-                        !string.IsNullOrEmpty(mailingAddressCountry) &&
-                        (mailingAddressProvince != null) &&
-                        !string.IsNullOrEmpty(mailingAddresPostalCode) &&
-                        corporateDetailsFilesExists;
-                    break;
-                default:
-                    isComplete = false;
-                    break;
-            }
-            return isComplete;
-        }
 
     }
 }
