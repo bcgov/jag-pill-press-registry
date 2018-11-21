d326eeeaee3bae241ce948de92356749b227ec3e
George Walker <george@walkersoftware.ca>
2018-11-21 12:33:11 -0800

    Business profile changes for demo

--- pill-press-app/ViewModels/Account.cs
+++ pill-press-app/ViewModels/Account.cs
@@ -73,19 +73,27 @@ namespace Gov.Jag.PillPressRegistry.Public.ViewModels
         public string businessPhoneNumber { get; set; } //dynamics = telephone1
         public string description { get; set; } //dynamics = description
 
+        public string physicalAddressName { get; set; } //dynamics = address1_name
+        public string physicalAddressLine1 { get; set; } //dynamics = address1_line1
+        public string physicalAddressLine2 { get; set; } //dynamics = address1_line1
+        public string physicalAddressCity { get; set; } //dynamics = address1_city
+        public string physicalAddressCountry { get; set; } //dynamics = address1_country
+        public string physicalAddressProvince { get; set; } //dynamics = address1_stateorprovince
+        public string physicalAddressPostalCode { get; set; } //dynamics = address1_postalcode
 
-        public string mailingAddressName { get; set; } //dynamics = address1_name
-        public string mailingAddressStreet { get; set; } //dynamics = address1_line1
-        public string mailingAddressCity { get; set; } //dynamics = address1_city
-        public string mailingAddressCountry { get; set; } //dynamics = address1_country
-        public string mailingAddressProvince { get; set; } //dynamics = address1_stateorprovince
-        public string mailingAddresPostalCode { get; set; } //dynamics = address1_postalcode
+        public string mailingAddressName { get; set; } //dynamics = address2_name
+        public string mailingAddressLine1 { get; set; } //dynamics = address2_line1
+        public string mailingAddressLine2 { get; set; } //dynamics = address2_line1
+        public string mailingAddressCity { get; set; } //dynamics = address2_city
+        public string mailingAddressCountry { get; set; } //dynamics = address2_country
+        public string mailingAddressProvince { get; set; } //dynamics = address2_stateorprovince
+        public string mailingAddressPostalCode { get; set; } //dynamics = address2_postalcode
         
         public ViewModels.Contact additionalContact { get; set; }
         public ViewModels.Contact primaryContact { get; set; }
 
-        public ViewModels.CustomAddress physicalAddress { get; set; }
-        public ViewModels.CustomAddress mailingAddress { get; set; }
+        //public ViewModels.CustomAddress physicalAddress { get; set; }
+        //public ViewModels.CustomAddress mailingAddress { get; set; }
 
         public string name { get; set; }
         public string pstNumber { get; set; }
