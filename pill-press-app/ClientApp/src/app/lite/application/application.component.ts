5a93a8dae44e02bf16f9cc10e5a05a39faa467f3
msehudi <msehudi@gmail.com>
2018-11-23 11:42:32 -0800

    Update to Angular 7 (#33)
    * waiver application partial
    
    * Update to Angular 7

--- pill-press-app/ClientApp/src/app/lite/application/application.component.ts
+++ pill-press-app/ClientApp/src/app/lite/application/application.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../app-state/models/app-state';
-import { Subscription } from 'rxjs/Subscription';
-import { Subject } from 'rxjs/Subject';
-import { Observable } from 'rxjs/Observable';
+import { Subscription ,  Subject ,  Observable } from 'rxjs';
 import { MatSnackBar, MatDialog } from '@angular/material';
 import * as currentApplicationActions from '../../app-state/actions/current-application.action';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -105,12 +103,12 @@ export class ApplicationComponent implements OnInit, OnDestroy {
         this.savedFormData = saveData;
         this.updateApplicationInStore();
         if (showProgress === true) {
-          this.snackBar.open('Application has been saved', 'Success', { duration: 2500, extraClasses: ['red-snackbar'] });
+          this.snackBar.open('Application has been saved', 'Success', { duration: 2500, panelClass: ['red-snackbar'] });
         }
       },
       err => {
         saveResult.next(false);
-        this.snackBar.open('Error saving Application', 'Fail', { duration: 3500, extraClasses: ['red-snackbar'] });
+        this.snackBar.open('Error saving Application', 'Fail', { duration: 3500, panelClass: ['red-snackbar'] });
         console.log('Error occured');
       });
 
@@ -219,7 +217,7 @@ export class ApplicationComponent implements OnInit, OnDestroy {
               this.router.navigate(['/dashboard']);
             },
             err => {
-              this.snackBar.open('Error cancelling the application', 'Fail', { duration: 3500, extraClasses: ['red-snackbar'] });
+              this.snackBar.open('Error cancelling the application', 'Fail', { duration: 3500, panelClass: ['red-snackbar'] });
               console.error('Error cancelling the application');
             });
         }
--- pill-press-app/ClientApp/src/app/services/contact-data.service.ts
+++ pill-press-app/ClientApp/src/app/services/contact-data.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 
 import { HttpHeaders, HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/toPromise';
+
 import { DynamicsContact } from '../models/dynamics-contact.model';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ContactDataService {
