085545c6ce94ac4279c457a2b81997eceb3dca1a
George Walker <george@walkersoftware.ca>
2018-11-26 11:22:34 -0800

    SPDPP-355 Custom Product API

---
+++ pill-press-app/DynamicsExtensions/CustomProduct.cs
@@ -0,0 +1,32 @@
+﻿
+using Gov.Jag.PillPressRegistry.Interfaces.Models;
+using Gov.Jag.PillPressRegistry.Public.Utils;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gov.Jag.PillPressRegistry.Interfaces
+{
+    public static class CustomProductDynamicsExtensions
+    {
+
+
+        public static MicrosoftDynamicsCRMbcgovCustomproduct GetCustomProductById(this IDynamicsClient system, Guid id)
+        {
+            MicrosoftDynamicsCRMbcgovCustomproduct result;
+            try
+            {
+                // fetch from Dynamics.
+                result = system.Customproducts.GetByKey(id.ToString());
+            }
+            catch (OdataerrorException)
+            {
+                result = null;
+            }
+            return result;
+        }
+
+        
+
+    }
+}
