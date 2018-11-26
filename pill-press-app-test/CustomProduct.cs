50724cf5db374123686e3ac88d4e389a1ea5bcd1
George Walker <george@walkersoftware.ca>
2018-11-26 11:22:34 -0800

    SPDPP-355 Custom Product API

---
+++ pill-press-app-test/CustomProduct.cs
@@ -0,0 +1,119 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace Gov.Jag.PillPressRegistry.Public.Test
+{
+    public class CustomProductTests : ApiIntegrationTestBaseWithLogin
+    {
+        public CustomProductTests(CustomWebApplicationFactory<Startup> factory)
+          : base(factory)
+        { }
+
+        const string service = "customproduct";
+        [Fact]
+        public async System.Threading.Tasks.Task TestNoAccessToAnonymousUser()
+        {
+            string id = "SomeRandomId";
+
+            // first confirm we are not logged in
+            await GetCurrentUserIsUnauthorized();
+
+            // try a random GET, should return unauthorized
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/" + service + "/" + id);
+            var response = await _client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            string _discard = await response.Content.ReadAsStringAsync();
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task TestCRUD()
+        {
+            string initialName = "InitialName";
+            string changedName = "ChangedName";
+
+            var loginUser = randomNewUserName("NewLoginUser", 6);
+            var strId = await LoginAndRegisterAsNewUser(loginUser);
+
+            // C - Create
+            var request = new HttpRequestMessage(HttpMethod.Post, "/api/" + service);
+
+            ViewModels.CustomProduct viewmodel_customproduct = new ViewModels.CustomProduct()
+            {
+                productdescriptionandintendeduse = initialName
+            };
+
+            string jsonString = JsonConvert.SerializeObject(viewmodel_customproduct);
+
+            request.Content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+            var response = await _client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            // parse as JSON.
+            jsonString = await response.Content.ReadAsStringAsync();
+            ViewModels.CustomProduct responseViewModel = JsonConvert.DeserializeObject<ViewModels.CustomProduct>(jsonString);
+
+            // name should match.
+            Assert.Equal(initialName, responseViewModel.productdescriptionandintendeduse);
+            Guid id = new Guid(responseViewModel.id);
+
+            // R - Read
+
+            request = new HttpRequestMessage(HttpMethod.Get, "/api/" + service + "/" + id);
+            response = await _client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            jsonString = await response.Content.ReadAsStringAsync();
+            responseViewModel = JsonConvert.DeserializeObject<ViewModels.CustomProduct>(jsonString);
+            Assert.Equal(initialName, responseViewModel.productdescriptionandintendeduse);
+
+            // U - Update            
+            ViewModels.CustomProduct patchModel = new ViewModels.CustomProduct()
+            {
+                productdescriptionandintendeduse = changedName
+            };
+
+            request = new HttpRequestMessage(HttpMethod.Put, "/api/" + service + "/" + id)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(patchModel), Encoding.UTF8, "application/json")
+            };
+            response = await _client.SendAsync(request);
+            jsonString = await response.Content.ReadAsStringAsync();
+            response.EnsureSuccessStatusCode();
+
+            // verify that the update persisted.
+
+            request = new HttpRequestMessage(HttpMethod.Get, "/api/" + service + "/" + id);
+            response = await _client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            jsonString = await response.Content.ReadAsStringAsync();
+
+            responseViewModel = JsonConvert.DeserializeObject<ViewModels.CustomProduct>(jsonString);
+            Assert.Equal(changedName, responseViewModel.productdescriptionandintendeduse);
+
+            // D - Delete
+
+            request = new HttpRequestMessage(HttpMethod.Post, "/api/" + service + "/" + id + "/delete");
+            response = await _client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            // second delete should return a 404.
+            request = new HttpRequestMessage(HttpMethod.Post, "/api/" + service + "/" + id + "/delete");
+            response = await _client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+
+            // should get a 404 if we try a get now.
+            request = new HttpRequestMessage(HttpMethod.Get, "/api/" + service + "/" + id);
+            response = await _client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+
+            await LogoutAndCleanupTestUser(strId);
+
+        }
+    }
+}
