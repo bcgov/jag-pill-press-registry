022a1eb062d224e435e25c65ba700034ea71aadf
Moffat Sehudi <msehudi@gmail.com>
2018-11-29 08:27:50 -0800

    scafold applicaiton components

---
+++ pill-press-app/ClientApp/src/app/owner-applications/authorized-owner/authorized-application/authorized-application.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authorized-application',
+  templateUrl: './authorized-application.component.html',
+  styleUrls: ['./authorized-application.component.css']
+})
+export class AuthorizedApplicationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
