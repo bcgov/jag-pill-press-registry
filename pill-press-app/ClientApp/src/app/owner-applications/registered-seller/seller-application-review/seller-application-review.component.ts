1567cb26de2fa9509e410568c1a54fd77818f5d1
msehudi <msehudi@gmail.com>
2018-11-29 09:53:55 -0800

    Authorized seller application partial (#42)
    * waiver application partial
    
    * waiver-application partial
    
    * waiver partial
    
    * scafold applicaiton components
    
    * authorized seller partial

---
+++ pill-press-app/ClientApp/src/app/owner-applications/registered-seller/seller-application-review/seller-application-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-application-review',
+  templateUrl: './seller-application-review.component.html',
+  styleUrls: ['./seller-application-review.component.css']
+})
+export class SellerApplicationReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
