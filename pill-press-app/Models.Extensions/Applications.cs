6e4cb1fb62b6fa0b69833117c5ff1788a4db460d
Moffat Sehudi <msehudi@gmail.com>
2018-11-29 12:10:03 -0800

    Authorized owner partial

--- pill-press-app/Models.Extensions/Applications.cs
+++ pill-press-app/Models.Extensions/Applications.cs
@@ -35,7 +35,7 @@ namespace Gov.Jag.PillPressRegistry.Public.Models
 
                 // Purpose of Controlled Equipment
                 result.producingownproduct = incident.BcgovProducingownproduct;
-                result.providingmanufacturingtoothers = incident.BcgovProvidingmanufacturingtoothers;                
+                result.providingmanufacturingtoothers = incident.BcgovProvidingmanufacturingtoothers;
 
                 // Business Details
                 result.mainbusinessfocus = incident.BcgovMainbusinessfocus;
@@ -45,6 +45,24 @@ namespace Gov.Jag.PillPressRegistry.Public.Models
                 result.declarationofcorrectinformation = incident.BcgovDeclarationofcorrectinformation;
                 result.foippaconsent = incident.BcgovFoippaconsent;
 
+                result.foodanddrugact = incident.BcgovFoodanddrugact;
+                result.legislativeauthorityother = incident.BcgovLegislativeauthorityother;
+                result.kindsofproductsdrugs = incident.BcgovKindsofproductsdrugs;
+                result.kindsofproductsnaturalhealthproducts = incident.BcgovKindsofproductsnaturalhealthproducts;
+                result.kindsofproductsother = incident.BcgovKindsofproductsother;
+                result.drugestablishmentlicence = incident.BcgovDrugestablishmentlicence;
+                result.sitelicence = incident.BcgovSitelicence;
+                result.otherlicence = incident.BcgovOtherlicence;
+                result.delbusinessname = incident.BcgovDelbusinessname;
+                result.drugestablishmentlicencenumber = incident.BcgovDrugestablishmentlicencenumber;
+                result.drugestablishmentlicenceexpirydate = incident.BcgovDrugestablishmentlicenceexpirydate;
+                result.sitelicencebusinessname = incident.BcgovSitelicencebusinessname;
+                result.sitelicencenumber = incident.BcgovSitelicencenumber;
+                result.sitelicenceexpirydate = incident.BcgovSitelicenceexpirydate;
+                result.otherlicencebusinessname = incident.BcgovOtherlicencebusinessname;
+                result.otherlicencenumber = incident.BcgovOtherlicencenumber;
+                result.otherlicenceexpirydate = incident.BcgovOtherlicenceexpirydate;
+
                 // CustomerID
                 if (incident.CustomeridAccount != null)
                 {
@@ -52,7 +70,7 @@ namespace Gov.Jag.PillPressRegistry.Public.Models
                 }
 
                 // Custom Products
-                if(incident?.BcgovIncidentCustomproductRelatedApplication?.Count > 0)
+                if (incident?.BcgovIncidentCustomproductRelatedApplication?.Count > 0)
                 {
                     result.CustomProducts = new List<CustomProduct>();
                     foreach (var product in incident.BcgovIncidentCustomproductRelatedApplication)
@@ -82,10 +100,28 @@ namespace Gov.Jag.PillPressRegistry.Public.Models
             to.BcgovMainbusinessfocus = from.mainbusinessfocus;
             to.BcgovManufacturingprocessdescription = from.manufacturingprocessdescription;
 
+             to.BcgovFoodanddrugact = from.foodanddrugact;
+             to.BcgovLegislativeauthorityother = from.legislativeauthorityother;
+             to.BcgovKindsofproductsdrugs = from.kindsofproductsdrugs;
+             to.BcgovKindsofproductsnaturalhealthproducts = from.kindsofproductsnaturalhealthproducts;
+             to.BcgovKindsofproductsother = from.kindsofproductsother;
+             to.BcgovDrugestablishmentlicence = from.drugestablishmentlicence;
+             to.BcgovSitelicence = from.sitelicence;
+             to.BcgovOtherlicence = from.otherlicence;
+             to.BcgovDelbusinessname = from.delbusinessname;
+             to.BcgovDrugestablishmentlicencenumber = from.drugestablishmentlicencenumber;
+             to.BcgovDrugestablishmentlicenceexpirydate = from.drugestablishmentlicenceexpirydate;
+             to.BcgovSitelicencebusinessname = from.sitelicencebusinessname;
+             to.BcgovSitelicencenumber = from.sitelicencenumber;
+             to.BcgovSitelicenceexpirydate = from.sitelicenceexpirydate;
+             to.BcgovOtherlicencebusinessname = from.otherlicencebusinessname;
+             to.BcgovOtherlicencenumber = from.otherlicencenumber;
+             to.BcgovOtherlicenceexpirydate = from.otherlicenceexpirydate;
+
             // Declarations and Consent
             to.BcgovDeclarationofcorrectinformation = from.declarationofcorrectinformation;
             to.BcgovFoippaconsent = from.foippaconsent;
-    }
+        }
 
     }
 }
