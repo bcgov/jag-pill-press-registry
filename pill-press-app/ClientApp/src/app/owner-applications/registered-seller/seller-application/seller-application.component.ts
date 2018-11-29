321096bfe3f4b197ca42cf18e93c949256a733b9
Moffat Sehudi <msehudi@gmail.com>
2018-11-29 08:27:50 -0800

    scafold applicaiton components

---
+++ pill-press-app/ClientApp/src/app/owner-applications/registered-seller/seller-application/seller-application.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-application',
+  templateUrl: './seller-application.component.html',
+  styleUrls: ['./seller-application.component.css']
+})
+export class SellerApplicationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
