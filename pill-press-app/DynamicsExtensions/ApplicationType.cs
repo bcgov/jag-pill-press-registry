9b63a53b101961e0258718c9e1286d9b74f3222b
George Walker <george@walkersoftware.ca>
2018-11-29 15:45:09 -0800

    Set application type to waiver. (#49)

---
+++ pill-press-app/DynamicsExtensions/ApplicationType.cs
@@ -0,0 +1,40 @@
+﻿
+using Gov.Jag.PillPressRegistry.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gov.Jag.PillPressRegistry.Interfaces
+{
+    public static class ApplicationTypeDynamicsExtensions
+    {
+        
+        public static string GetApplicationTypeIdByName(this IDynamicsClient system, string name)
+        {
+            string result = null;
+            name = name.Replace("'", "''");
+
+            MicrosoftDynamicsCRMbcgovApplicationtype applicationType = null;
+
+            try
+            {
+                var accountResponse = system.Applicationtypes.Get(filter: $"bcgov_name eq '{name}'");
+                applicationType = accountResponse.Value.FirstOrDefault();
+            }
+            catch (Exception)
+            {
+                applicationType = null;
+            }
+
+            // get the primary contact.
+            if (applicationType != null)
+            {
+                result = applicationType.BcgovApplicationtypeid;
+            }
+
+            return result;
+        }
+
+
+    }
+}
