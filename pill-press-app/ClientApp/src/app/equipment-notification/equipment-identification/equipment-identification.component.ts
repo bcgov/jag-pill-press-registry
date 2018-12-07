6c5b478969aab82f7f98a7963fafa825f6ebc7ca
Moffat Sehudi <msehudi@gmail.com>
2018-12-07 11:53:03 -0800

    Equipment identification partial

--- pill-press-app/ClientApp/src/app/equipment-notification/equipment-identification/equipment-identification.component.ts
+++ pill-press-app/ClientApp/src/app/equipment-notification/equipment-identification/equipment-identification.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-equipment-identification',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment-identification.component.scss']
 })
 export class EquipmentIdentificationComponent implements OnInit {
+  form: FormGroup;
+  busy: Subscription;
+  equipmentId: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder) {
+    this.equipmentId = this.route.snapshot.params.id;
+  }
 
   ngOnInit() {
+    this.form = this.fb.group({
+
+    });
+
+  }
+
+  markAsTouched() {
+
+  }
+
+  save(goToNextForm: boolean) {
+    if (goToNextForm) {
+      this.router.navigateByUrl(`/equipment-notification/source/${this.equipmentId}`);
+    } else {
+      this.router.navigateByUrl('/dashboard');
+    }
   }
 
 }
