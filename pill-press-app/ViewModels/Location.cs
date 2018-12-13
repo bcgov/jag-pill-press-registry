23a56802eb064a681adeffa026631e0bb6a53a8c
George Walker <george@walkersoftware.ca>
2018-12-13 11:31:09 -0800

    Support for equipment location

---
+++ pill-press-app/ViewModels/Location.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Gov.Jag.PillPressRegistry.Public.ViewModels
+{
+    public class Location
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "privateDwelling")]
+        public bool? PrivateDwelling { get; set; }
+
+        [JsonProperty(PropertyName = "settingDescription")]
+        public string SettingDescription { get; set; }
+
+        [JsonProperty(PropertyName = "address")]
+        public CustomAddress Address { get; set; }
+
+        [JsonProperty(PropertyName = "application")]
+        public Application Application { get; set; }
+
+    }
+}
