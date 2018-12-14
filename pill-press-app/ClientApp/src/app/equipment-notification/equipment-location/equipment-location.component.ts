5d8aea3f2de42f9888312fcaa86c83d716fc3838
Moffat Sehudi <msehudi@gmail.com>
2018-12-14 08:27:02 -0800

    Equipment location binding

--- pill-press-app/ClientApp/src/app/equipment-notification/equipment-location/equipment-location.component.ts
+++ pill-press-app/ClientApp/src/app/equipment-notification/equipment-location/equipment-location.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Subscription, zip } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ApplicationDataService } from '../../services/adoxio-application-data.service';
+import { Application } from '../../models/application.model';
+import { EquipmentLocation } from '../../models/equipment-location.model';
 
 @Component({
   selector: 'app-equipment-location',
@@ -12,9 +15,11 @@ export class EquipmentLocationComponent implements OnInit {
   form: FormGroup;
   busy: Subscription;
   equipmentId: string;
+  busyPromise: Promise<any>;
 
   constructor(private route: ActivatedRoute,
     private router: Router,
+    private applicationDataService: ApplicationDataService,
     private fb: FormBuilder) {
     this.equipmentId = this.route.snapshot.params.id;
   }
@@ -22,27 +27,55 @@ export class EquipmentLocationComponent implements OnInit {
   ngOnInit() {
     this.form = this.fb.group({
       id: [],
-      outsideBcSellersAddress: this.fb.group({
+      equipmentLocation: this.fb.group({
         id: [],
-        streetLine1: [],
-        streetLine2: [],
-        city: [],
-        province: ['British Columbia'],
-        postalCode: [],
+        address: this.fb.group({
+          id: [],
+          streetLine1: [],
+          streetLine2: [],
+          city: [],
+          province: ['British Columbia'],
+          postalCode: [],
+        }),
+        privateDwelling: [],
+        settingDescription: [],
       }),
     });
 
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.busy = this.applicationDataService.getApplicationById(this.equipmentId)
+      .subscribe((data: Application) => {
+        data.equipmentLocation = data.equipmentLocation || <EquipmentLocation>{ address: {} };
+        this.form.patchValue(data);
+      }, error => {
+        // debugger;
+      });
   }
 
   markAsTouched() {
 
   }
 
-  save(goToNextForm: boolean) {
-    if (goToNextForm) {
-      this.router.navigateByUrl(`/equipment-notification/review/${this.equipmentId}`);
-    } else {
-      this.router.navigateByUrl('/dashboard');
+
+  save(goToReview: boolean) {
+    if (this.form.valid || goToReview === false) {
+      const value = this.form.value;
+      const saveList = [this.applicationDataService.updateApplication(value)];
+      this.busyPromise = zip(...saveList)
+        .toPromise()
+        .then(res => {
+          if (goToReview) {
+            this.router.navigateByUrl(`/equipment-notification/review/${this.equipmentId}`);
+          } else {
+            this.router.navigateByUrl(`/dashboard`);
+            // this.reloadData();
+          }
+        }, err => {
+          // todo: show errors;
+        });
     }
   }
 
