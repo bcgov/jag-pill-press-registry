d6e70ab3adf626ac86cb9dedfd89c8b032954ee7
Moffat Sehudi <msehudi@gmail.com>
2018-12-14 10:02:11 -0800

    dashboard and equipment notification partial

--- pill-press-app/ClientApp/src/app/equipment-notification/equipment-review/equipment-review.component.ts
+++ pill-press-app/ClientApp/src/app/equipment-notification/equipment-review/equipment-review.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute, Router } from '@angular/router';
+import { Application } from '../../models/application.model';
+import { ApplicationDataService } from '../../services/adoxio-application-data.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { zip } from 'rxjs';
+
 @Component({
   selector: 'app-equipment-review',
   templateUrl: './equipment-review.component.html',
   styleUrls: ['./equipment-review.component.scss']
 })
 export class EquipmentReviewComponent implements OnInit {
+  busy: any;
+  equipmentId: string;
+  notification: Application;
+  form: FormGroup;
+  busyPromise: Promise<any>;
 
-  constructor() { }
+  constructor(private applicationDataService: ApplicationDataService,
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute) {
+    this.equipmentId = this.route.snapshot.params.id;
+   }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      id: [],
+      declarationofcorrectinformation: [],
+    });
+
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.busy = this.applicationDataService.getApplicationById(this.equipmentId)
+      .subscribe((data: Application) => {
+        this.notification = data;
+        this.form.patchValue(data);
+      }, error => {
+        // debugger;
+      });
+  }
+
+  save(goToReview: boolean) {
+    if (this.form.valid || goToReview === false) {
+      const value = this.form.value;
+      const saveList = [this.applicationDataService.updateApplication(value)];
+      this.busyPromise = zip(...saveList)
+        .toPromise()
+        .then(res => {
+          if (goToReview) {
+            this.router.navigateByUrl(`/equipment-notification/thank-you/${this.equipmentId}`);
+          } else {
+            this.router.navigateByUrl(`/dashboard`);
+            // this.reloadData();
+          }
+        }, err => {
+          // todo: show errors;
+        });
+    }
   }
 
 }
