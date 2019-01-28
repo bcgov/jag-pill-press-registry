9b98a670c03d223b675692da4a4bc6ef26690933
Moffat Sehudi <msehudi@gmail.com>
2019-01-28 12:42:01 -0800

    SPDPP-729 Change Location

---
+++ pill-press-app/ClientApp/src/app/equipment-changes/report-changes/report-changes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-changes',
+  templateUrl: './report-changes.component.html',
+  styleUrls: ['./report-changes.component.scss']
+})
+export class ReportChangesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
