3c9ea54692c9e2bdc57113d642966ba69aa40e20
msehudi <msehudi@gmail.com>
2019-02-15 08:57:25 -0800

    Develop: fixes (#206)
    * LSD and no external assets
    
    * font awesome, npm
    
    * use fontawesome from npm
    
    * remove local fontawesome dependencies
    
    * Report of Sale Review partial
    
    * reporting sales review partial
    
    * Equipment changes review partial
    
    * buid fix
    
    * Reporting Sales and LSD fixes
    
    * filter fix

--- pill-press-app/ClientApp/src/app/equipment-changes/report-changes/report-changes.component.ts
+++ pill-press-app/ClientApp/src/app/equipment-changes/report-changes/report-changes.component.ts
@@ -46,7 +46,9 @@ export class ReportChangesComponent implements OnInit {
             this.stepper.reset();
           }
           this.tab = this.route.snapshot.firstChild.url[0].path;
-          this.stepper.selectedIndex = this.tabList.indexOf(this.tab);
+          if (this.tabList.indexOf(this.tab) !== -1) {
+            this.stepper.selectedIndex = this.tabList.indexOf(this.tab);
+          }
         }
       }
     });
@@ -55,7 +57,7 @@ export class ReportChangesComponent implements OnInit {
 
 
   selectionChange(event) {
-    this.router.navigateByUrl(`/equipment-changes/report-changes/${this.tabList[event.selectedIndex]}/${this.equipmentId}`);
+    this.router.navigateByUrl(`/equipment-changes/reporting-changes/${this.tabList[event.selectedIndex]}/${this.equipmentId}`);
   }
 
 }
