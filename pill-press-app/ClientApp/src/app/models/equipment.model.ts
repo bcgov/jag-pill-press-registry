009f992b38aa265031a06fba6ed730f1dbc8f74f
Rafael Ponce de Leon <36902894+rafaelponcedeleon@users.noreply.github.com>
2019-03-21 06:48:12 -0700

    SPDPP-933 - Filter completed equipment that doesn't belong to this business profile (account) anymore (#223) (#224)
    * Dashboard - Owner Categories updates for renewals
    
    * SPDPP-759, SPDPP-882, SPDPP-883, SPDPP-887
    
    * SPDPP-951, SPDPP-902 reapplication statuses, SPDPP-888
    
    * layout and spelling/text changes
    
    * In progress: dashboard - equipment status changes
    
    * SPDPP-850, SPDPP-902, SPDPP-965
    
    * Added comment TODO changes to fix the “GetOkResponseModelModelModel…” generated models
    
    * Adjustment to dashboard - in progress tab table
    
    * SPDPP-933 - Filter completed equipment that doesn't belong to this business profile (account) anymore

--- pill-press-app/ClientApp/src/app/models/equipment.model.ts
+++ pill-press-app/ClientApp/src/app/models/equipment.model.ts
@@ -1,20 +1,21 @@
 
 
 export interface Equipment {
-    id: string;
-    equipmentType: string;
-    equipmentTypeOther: string;
-    name: string;
-    pillpressEncapsulatorSize: string;
-    pillpressEncapsulatorSizeOther: string;
-    levelOfEquipmentAutomation: string;
-    pillpressMaxCapacity: Number;
-    howWasEquipmentBuilt: string;
-    HhwWasEquipmentBuiltOther: string;
-    nameOfManufacturer: string;
-    equipmentMake: string;
-    equipmentModel: string;
-    serialNumber: string;
-    encapsulatorMaxCapacity: Number;
-    customBuiltSerialNumber: string;
+  id: string;
+  equipmentType: string;
+  equipmentTypeOther: string;
+  name: string;
+  pillpressEncapsulatorSize: string;
+  pillpressEncapsulatorSizeOther: string;
+  levelOfEquipmentAutomation: string;
+  pillpressMaxCapacity: Number;
+  howWasEquipmentBuilt: string;
+  HhwWasEquipmentBuiltOther: string;
+  nameOfManufacturer: string;
+  equipmentMake: string;
+  equipmentModel: string;
+  serialNumber: string;
+  encapsulatorMaxCapacity: Number;
+  customBuiltSerialNumber: string;
+  bcgovCurrentBusinessOwner: Account;
 }
