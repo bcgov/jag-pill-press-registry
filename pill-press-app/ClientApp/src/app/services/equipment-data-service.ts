0f5c9a7e987eecff5ef1de5a61a9736a6a80cad7
Rafael Ponce de Leon <36902894+rafaelponcedeleon@users.noreply.github.com>
2019-04-04 11:29:48 -0700

    Equipment changes in progress (#230)
    * Update Readme.MD
    
    * Update Readme.MD
    
    * Dashboard - Owner Categories updates for renewals
    
    * SPDPP-759, SPDPP-882, SPDPP-883, SPDPP-887
    
    * SPDPP-951, SPDPP-902 reapplication statuses, SPDPP-888
    
    * layout and spelling/text changes
    
    * In progress: dashboard - equipment status changes
    
    * SPDPP-850, SPDPP-902, SPDPP-965
    
    * Added comment TODO changes to fix the “GetOkResponseModelModelModel…” generated models
    
    * Adjustment to dashboard - in progress tab table
    
    * SPDPP-933 - Filter completed equipment that doesn't belong to this business profile (account) anymore
    
    *  SPDPP-933 - Filter completed equipment that doesn't belong to this business profile (account) anymore (#223) (#224)
    
    * Dashboard - Owner Categories updates for renewals
    
    * SPDPP-759, SPDPP-882, SPDPP-883, SPDPP-887
    
    * SPDPP-951, SPDPP-902 reapplication statuses, SPDPP-888
    
    * layout and spelling/text changes
    
    * In progress: dashboard - equipment status changes
    
    * SPDPP-850, SPDPP-902, SPDPP-965
    
    * Added comment TODO changes to fix the “GetOkResponseModelModelModel…” generated models
    
    * Adjustment to dashboard - in progress tab table
    
    * SPDPP-933 - Filter completed equipment that doesn't belong to this business profile (account) anymore
    
    * SPDPP-969
    
    * SPDPP-886
    
    * SPDPP-902, SPDPP-901
    
    * SPDPP-886
    
    * SPDPP-890
    
    * SPDPP-782
    
    * SDPP-900 validation messages, link to document upload question
    
    * SPDPP-900 field validation
    
    * SPDPP-900
    
    * Removed private declaration of method
    
    * private dwelling changes part 1
    
    * manual fixes to the autorest for provate dwelling field
    
    * changes to fix the “GetOkResponseModelModelModel…” generated models
    
    * Equipment location changes (location drop down and private dwelling)
    
    * New autorest generation with fix to response model naming
    
    * location change (in progress)
    
    * location change in progress

--- pill-press-app/ClientApp/src/app/services/equipment-data-service.ts
+++ pill-press-app/ClientApp/src/app/services/equipment-data-service.ts
@@ -9,10 +9,8 @@ import { DynamicsAccount } from '../models/dynamics-account.model';
 export class EquipmentDataService {
 
   apiPath = 'api/equipment/';
-  headers: HttpHeaders = new HttpHeaders({
-    'Content-Type': 'application/json'
-  });
-
+  headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+  temp: Observable<any>;
 
   constructor(private http: HttpClient) { }
 
@@ -22,6 +20,16 @@ export class EquipmentDataService {
    */
   public getEquipment(equipmentId: string): Observable<Equipment> {
     return this.http.get<Equipment>(this.apiPath + equipmentId, { headers: this.headers });
+  }
+
+  /**
+   * Change Equipment Location
+   * @param applicationData
+   */
+  public changeEquipmentLocation(applicationData: any): Observable<Equipment> {
+    var temp = applicationData;
+    //return temp;
+    return this.http.put<Equipment>(this.apiPath + applicationData.id + '/changeEquipmentLocation', applicationData, { headers: this.headers });
   }
 
 }
