9c5ec293df830e8985ce50716a1f58ac45a0122b
George Walker <george@walkersoftware.ca>
2019-09-06 14:56:00 -0700

    Updated OpenShift configurations (#274)
    * SPDPP-859 fix for save location issue
    
    * SPDPP-864 fix for issue with add location
    
    * Code for generation of Dynamics Client
    
    * Updated OpenShift configurations

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,5 +1,5 @@
 # bash script to be sourced to set environment variables for OpenShift scripts
-export PROJECT_NAMESPACE="rqnyx8"
+export PROJECT_NAMESPACE="crraem"
 export PROJECT_OS_DIR=${PROJECT_OS_DIR:-../../openshift}
 
 # The templates that should not have their GIT referances(uri and ref) over-ridden
