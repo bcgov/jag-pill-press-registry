068ebdb35af130215c5f89e6f15dffdb297550e9
Rafael Ponce de Leon <Rafael.PonceDeLeon@quartech.com>
2020-01-29 17:39:02 -0800

    Add odata-version: 3.0 to request Header in oder to have the SharePoint folder creation working

--- pill-press-interfaces/SharePoint/SharePointFileManager.cs
+++ pill-press-interfaces/SharePoint/SharePointFileManager.cs
@@ -287,6 +287,8 @@ namespace Gov.Jag.PillPressRegistry.Interfaces
             StringContent strContent = new StringContent(jsonString, Encoding.UTF8);
             strContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json;odata=verbose");
             endpointRequest.Content = strContent;
+            //add odata-version: 3.0, otherwise by default odata-version is 4.0 and an error will be thrown.
+            endpointRequest.Headers.TryAddWithoutValidation("odata-version", "3.0");
 
             // make the request.
             var response = await _Client.SendAsync(endpointRequest);
