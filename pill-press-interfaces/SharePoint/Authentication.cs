8dd100168734af687fc1fcdb4a96bb9c7bfbc0fe
Nick Phura <nickphura@gmail.com>
2026-01-23 12:54:58 -0800

    Update Dynamics and SharePoint to support cloud auth/api.

--- pill-press-interfaces/SharePoint/Authentication.cs
+++ pill-press-interfaces/SharePoint/Authentication.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
@@ -10,23 +9,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Xml;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace Gov.Jag.PillPressRegistry.Interfaces
 {
-
     /*
      *  SharePoint Authentication functions.
-     *  
+     *
      *  The following references were used for these functions:
-     *  
+     *
      *  https://github.com/SharePoint/PnP/tree/master/Solutions/AspNetCore.Authentication/src
      *  https://github.com/jwillmer/SharePointAuthentication/blob/master/SharePointAuthenticationSkeleton/SharePointAuthenticationSkeleton/Helpers/SharePointAuthentication.cs
-     *
      */
-
     static class Authentication
     {
-
         private static string GetXMLInnerText(XmlDocument doc, string tagName)
         {
             string result = "";
@@ -54,7 +50,9 @@ namespace Gov.Jag.PillPressRegistry.Interfaces
 
             string notBefore = GetXMLInnerText(samlAssertion, "wsu:Created");
             string notOnOrAfter = GetXMLInnerText(samlAssertion, "wsu:Expires");
-            var requestedTokenRaw = samlAssertion.GetElementsByTagName("t:RequestedSecurityToken")[0];
+            var requestedTokenRaw = samlAssertion.GetElementsByTagName("t:RequestedSecurityToken")[
+                0
+            ];
 
             var requestedTokenData = samlAssertion.ImportNode(requestedTokenRaw, true);
 
@@ -63,43 +61,93 @@ namespace Gov.Jag.PillPressRegistry.Interfaces
             requestedTokenXml.LoadXml(requestedTokenData.InnerXml);
 
             XmlDocument soapMessage = new XmlDocument();
-            XmlElement soapEnvelope = soapMessage.CreateElement("t", "RequestSecurityTokenResponse", "http://schemas.xmlsoap.org/ws/2005/02/trust");
+            XmlElement soapEnvelope = soapMessage.CreateElement(
+                "t",
+                "RequestSecurityTokenResponse",
+                "http://schemas.xmlsoap.org/ws/2005/02/trust"
+            );
             soapMessage.AppendChild(soapEnvelope);
-            XmlElement lifeTime = soapMessage.CreateElement("t", "Lifetime", soapMessage.DocumentElement.NamespaceURI);
+            XmlElement lifeTime = soapMessage.CreateElement(
+                "t",
+                "Lifetime",
+                soapMessage.DocumentElement.NamespaceURI
+            );
             soapEnvelope.AppendChild(lifeTime);
-            XmlElement created = soapMessage.CreateElement("wsu", "Created", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
+            XmlElement created = soapMessage.CreateElement(
+                "wsu",
+                "Created",
+                "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
+            );
             XmlText createdValue = soapMessage.CreateTextNode(notBefore);
             created.AppendChild(createdValue);
             lifeTime.AppendChild(created);
-            XmlElement expires = soapMessage.CreateElement("wsu", "Expires", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
+            XmlElement expires = soapMessage.CreateElement(
+                "wsu",
+                "Expires",
+                "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
+            );
             XmlText expiresValue = soapMessage.CreateTextNode(notOnOrAfter);
             expires.AppendChild(expiresValue);
             lifeTime.AppendChild(expires);
-            XmlElement appliesTo = soapMessage.CreateElement("wsp", "AppliesTo", "http://schemas.xmlsoap.org/ws/2004/09/policy");
+            XmlElement appliesTo = soapMessage.CreateElement(
+                "wsp",
+                "AppliesTo",
+                "http://schemas.xmlsoap.org/ws/2004/09/policy"
+            );
             soapEnvelope.AppendChild(appliesTo);
-            XmlElement endPointReference = soapMessage.CreateElement("wsa", "EndpointReference", "http://www.w3.org/2005/08/addressing");
+            XmlElement endPointReference = soapMessage.CreateElement(
+                "wsa",
+                "EndpointReference",
+                "http://www.w3.org/2005/08/addressing"
+            );
             appliesTo.AppendChild(endPointReference);
-            XmlElement address = soapMessage.CreateElement("wsa", "Address", endPointReference.NamespaceURI);
+            XmlElement address = soapMessage.CreateElement(
+                "wsa",
+                "Address",
+                endPointReference.NamespaceURI
+            );
             XmlText addressValue = soapMessage.CreateTextNode(relyingPartyIdentifier);
             address.AppendChild(addressValue);
             endPointReference.AppendChild(address);
-            XmlElement requestedSecurityToken = soapMessage.CreateElement("t", "RequestedSecurityToken", soapMessage.DocumentElement.NamespaceURI);
+            XmlElement requestedSecurityToken = soapMessage.CreateElement(
+                "t",
+                "RequestedSecurityToken",
+                soapMessage.DocumentElement.NamespaceURI
+            );
 
             XmlText createdRstValue = soapMessage.CreateTextNode("[RST]");
 
             requestedSecurityToken.AppendChild(createdRstValue);
 
             soapEnvelope.AppendChild(requestedSecurityToken);
-            XmlElement tokenType = soapMessage.CreateElement("t", "TokenType", soapMessage.DocumentElement.NamespaceURI);
-            XmlText tokenTypeValue = soapMessage.CreateTextNode("urn:oasis:names:tc:SAML:1.0:assertion");
+            XmlElement tokenType = soapMessage.CreateElement(
+                "t",
+                "TokenType",
+                soapMessage.DocumentElement.NamespaceURI
+            );
+            XmlText tokenTypeValue = soapMessage.CreateTextNode(
+                "urn:oasis:names:tc:SAML:1.0:assertion"
+            );
             tokenType.AppendChild(tokenTypeValue);
             soapEnvelope.AppendChild(tokenType);
-            XmlElement requestType = soapMessage.CreateElement("t", "RequestType", soapMessage.DocumentElement.NamespaceURI);
-            XmlText requestTypeValue = soapMessage.CreateTextNode("http://schemas.xmlsoap.org/ws/2005/02/trust/Issue");
+            XmlElement requestType = soapMessage.CreateElement(
+                "t",
+                "RequestType",
+                soapMessage.DocumentElement.NamespaceURI
+            );
+            XmlText requestTypeValue = soapMessage.CreateTextNode(
+                "http://schemas.xmlsoap.org/ws/2005/02/trust/Issue"
+            );
             requestType.AppendChild(requestTypeValue);
             soapEnvelope.AppendChild(requestType);
-            XmlElement keyType = soapMessage.CreateElement("t", "KeyType", soapMessage.DocumentElement.NamespaceURI);
-            XmlText keyTypeValue = soapMessage.CreateTextNode("http://schemas.xmlsoap.org/ws/2005/05/identity/NoProofKey");
+            XmlElement keyType = soapMessage.CreateElement(
+                "t",
+                "KeyType",
+                soapMessage.DocumentElement.NamespaceURI
+            );
+            XmlText keyTypeValue = soapMessage.CreateTextNode(
+                "http://schemas.xmlsoap.org/ws/2005/05/identity/NoProofKey"
+            );
             keyType.AppendChild(keyTypeValue);
             soapEnvelope.AppendChild(keyType);
 
@@ -108,9 +156,13 @@ namespace Gov.Jag.PillPressRegistry.Interfaces
             return result;
         }
 
-
-
-        public static async Task GetFedAuth(string samlSite, string token, string relyingPartyIdentifier, HttpClient client, CookieContainer cookieContainer)
+        public static async Task GetFedAuth(
+            string samlSite,
+            string token,
+            string relyingPartyIdentifier,
+            HttpClient client,
+            CookieContainer cookieContainer
+        )
         {
             // Encoding.UTF8.GetString(token.Token, 0, token.Token.Length)
             string samlToken = WrapInSoapMessage(token, relyingPartyIdentifier);
@@ -126,25 +178,33 @@ namespace Gov.Jag.PillPressRegistry.Interfaces
             };
 
             // create the body of the POST
-            string stringData = $"wa=wsignin1.0&wctx={HttpUtility.UrlEncode(sharepointSite.Wctx)}&wresult={HttpUtility.UrlEncode(samlToken)}";
+            string stringData =
+                $"wa=wsignin1.0&wctx={HttpUtility.UrlEncode(sharepointSite.Wctx)}&wresult={HttpUtility.UrlEncode(samlToken)}";
 
-            var content = new StringContent(stringData, Encoding.UTF8, "application/x-www-form-urlencoded");
+            var content = new StringContent(
+                stringData,
+                Encoding.UTF8,
+                "application/x-www-form-urlencoded"
+            );
 
             var _httpPostResponse = await client.PostAsync(sharepointSite.Wreply, content);
 
             var cookieUri = new Uri(sharepointSite.Wreply);
 
             // if we are using an API gateway we need to restructure the fedAuth cookie.
-            if (!string.Equals(sharepointSite.Wreply, $"{cookieUri.Scheme}://{cookieUri.Authority}"))
+            if (
+                !string.Equals(sharepointSite.Wreply, $"{cookieUri.Scheme}://{cookieUri.Authority}")
+            )
             {
                 var cookies = cookieContainer.GetCookies(cookieUri);
                 string fedAuthCookieValue = cookies["FedAuth"].Value;
 
-                cookieContainer.Add(new Uri($"{cookieUri.Scheme}://{cookieUri.Authority}"), new Cookie("FedAuth", fedAuthCookieValue, "/"));
+                cookieContainer.Add(
+                    new Uri($"{cookieUri.Scheme}://{cookieUri.Authority}"),
+                    new Cookie("FedAuth", fedAuthCookieValue, "/")
+                );
             }
-
         }
-
 
         /// <summary>
         /// Create a SOAP request for a SAML token
@@ -154,9 +214,15 @@ namespace Gov.Jag.PillPressRegistry.Interfaces
         /// <param name="password"></param>
         /// <param name="toUrl"></param>
         /// <returns></returns>
-        private static string ParameterizeSoapRequestTokenMsgWithUsernamePassword(string url, string username, string password, string toUrl)
+        private static string ParameterizeSoapRequestTokenMsgWithUsernamePassword(
+            string url,
+            string username,
+            string password,
+            string toUrl
+        )
         {
-            string samlRTString = "<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:a=\"http://www.w3.org/2005/08/addressing\" xmlns:u=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">"
+            string samlRTString =
+                "<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:a=\"http://www.w3.org/2005/08/addressing\" xmlns:u=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">"
                 + "<s:Header>"
                 + "<a:Action s:mustUnderstand=\"1\">http://schemas.xmlsoap.org/ws/2005/02/trust/RST/Issue</a:Action>"
                 + "<a:ReplyTo><a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address></a:ReplyTo>"
@@ -168,7 +234,6 @@ namespace Gov.Jag.PillPressRegistry.Interfaces
                 + "<t:RequestType>http://schemas.xmlsoap.org/ws/2005/02/trust/Issue</t:RequestType><t:TokenType>urn:oasis:names:tc:SAML:1.0:assertion</t:TokenType>"
                 + "</t:RequestSecurityToken></s:Body></s:Envelope>";
 
-
             samlRTString = samlRTString.Replace("[username]", username);
             samlRTString = samlRTString.Replace("[password]", password);
             samlRTString = samlRTString.Replace("[url]", url);
@@ -177,31 +242,39 @@ namespace Gov.Jag.PillPressRegistry.Interfaces
             return samlRTString;
         }
 
-        public async static Task<string> GetStsSamlToken(string spSiteUrl, string username, string password, string stsUrl)
+        public static async Task<string> GetStsSamlToken(
+            string spSiteUrl,
+            string username,
+            string password,
+            string stsUrl,
+            HttpClient client
+        )
         {
-            // Makes a request that conforms with the WS-Trust standard to 
-            // the Security Token Service to get a SAML security token back 
+            // Makes a request that conforms with the WS-Trust standard to
+            // the Security Token Service to get a SAML security token back
 
-
-            // generate the WS-Trust security token request SOAP message 
+            // generate the WS-Trust security token request SOAP message
             string saml11RT = ParameterizeSoapRequestTokenMsgWithUsernamePassword(
-                    spSiteUrl,
-                    username,
-                    password,
-                    stsUrl);
+                spSiteUrl,
+                username,
+                password,
+                stsUrl
+            );
 
             string response = null;
 
             if (saml11RT != null)
             {
-                var client = new HttpClient();
-                var content = new StringContent(saml11RT, System.Text.Encoding.UTF8, "application/soap+xml");
+                var content = new StringContent(
+                    saml11RT,
+                    System.Text.Encoding.UTF8,
+                    "application/soap+xml"
+                );
                 var result = await client.PostAsync(stsUrl, content);
                 response = await result.Content.ReadAsStringAsync();
             }
 
             return response;
         }
-
     }
 }
---
+++ pill-press-interfaces/SharePoint/SharePointConstants.cs
@@ -0,0 +1,20 @@
+namespace Gov.Jag.PillPressRegistry.Interfaces
+{
+    /// <summary>
+    /// SharePoint constants.
+    /// </summary>
+    /// <remarks>
+    /// Currently just a list of root level document library (folder) names + displayNames.
+    /// </remarks>
+    public static class SharePointConstants
+    {
+        public const string AccountFolderDisplayName = "Account";
+        public const string AccountFolderInternalName = "account";
+
+        public const string ApplicationFolderDisplayName = "incident";
+        public const string ApplicationFolderInternalName = "incident";
+
+        public const string ContactFolderDisplayName = "contact";
+        public const string ContactFolderInternalName = "contact";
+    }
+}
